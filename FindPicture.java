68780b9dc8f8e0b277aaae258c895f18ced3d706
castiellll <sofiane.adjiri@u-psud.fr>
2015-02-12 22:17:12 +0100

    testez

--- FindPicture.java
+++ FindPicture.java
@@ -1,30 +1,46 @@
 package Pictures;
 
+
+import Suchi.Game;
+
+import java.awt.AWTException;
 import java.awt.Point;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+
 
 public class FindPicture {
+	// source qui est l'image du screen et img et celle qu'on doit trouver dans source 
 	public BufferedImage source  , img ; 
-	
-	public  void loadPictures(String pathSrc , String pathImg){
-		File fSrc = new File (pathSrc);
+	// dot est le point des coordonées 
+	public Point dot  ; 
+
+	// constructeur on doit donc y mettre une buffered image qui sera le screen et le path du sprite à chercher
+	public FindPicture(BufferedImage src , String img){
+		this.loadPictures(img) ;
+		this.source = src ;
+		this.dot=new Point(-1,-1);
+	}
+
+	// loader le sprite dans le buffer img 
+	public  void loadPictures( String pathImg){
+		//File fSrc = new File (pathSrc);
 		File fImg = new File (pathImg);
 		try {
-			source = ImageIO.read(fSrc);
-			img = ImageIO.read(fImg);
+			//source = ImageIO.read(fSrc);
+			this.img = ImageIO.read(fImg);
 		} catch (IOException e) {
 			throw new RuntimeException("impossible to load image");
 		}
 	}
-
+	
+	//renvoie une array list qui contient la ligne "line" de l'image "img"
 	public   ArrayList<Integer> getLine(BufferedImage img , int line ){
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		for (int i = 0; i < img.getWidth(); i++) {
@@ -34,31 +50,89 @@ public class FindPicture {
 		}
 		return l ; 
 	}
+	
+	// meme principe mais pour les colonnes 
+	public ArrayList<Integer> getColumn(BufferedImage img , int column){
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		for(int i = 0 ; i<img.getHeight();i++){
+			c.add(img.getRGB(column, i));
+		}
+		return c ; 
+	}
+
+	// la on check si on trouve la colonne dans la grosse image 
+	public boolean checkColumn (BufferedImage source , BufferedImage img ){
+		ArrayList<Integer> columnImg = getColumn(img,img.getHeight()/2);// une colonne de l'image qu'oin cherche
+		ArrayList<Integer> columnSrc = new ArrayList<Integer>();//representera les colonnes de l'image source 
+		for(int i = 0 ; i<source.getWidth();i++){
+			columnSrc = getColumn(source,i);
+			if(columnSrc.containsAll(columnImg)){
+				dot.x=i;
+				return true ; 
+			}
+		}
+		return false ; 
+	}
 
 
-	public  Point check(BufferedImage source , BufferedImage img){
-		 Point dot = new Point(-1,-1);
-		ArrayList<Integer> lineImg = getLine(img,10);// une ligne de l'image qu'oin cherche
+	// meme principe mais pour les lignes 
+	public  boolean checkLine(BufferedImage source , BufferedImage img){
+		ArrayList<Integer> lineImg = getLine(img,img.getHeight()/2);// une ligne de l'image qu'oin cherche
 		ArrayList<Integer> lineSrc = new ArrayList<Integer>();//representera les lignes de l'image source 
 
 		for (int j = 0; j < source.getHeight(); j++) {
 			lineSrc = getLine(source,j);// retirer les lignes de l'image source 
 			if(lineSrc.containsAll(lineImg)){
 				dot.y=j;
-				dot.x=lineImg.size()/2;
+				return true ; 
 			}
 		}
 
-		return dot; 
+		return false; 
 	}
-	
-	public BufferedImage getSrc(){return this.source;}
-	public BufferedImage getImg(){return this.img;}
 
-	public static void main (String [] args){
-		FindPicture test = new FindPicture();
-		test.loadPictures("/Users/sofiane/desktop/screenshot.png","/Users/sofiane/desktop/screenshot9.png");
-		System.out.println(" is it working ?? "+test.check(test.getSrc(), test.getImg()));
+	///////////////////////////////
+	// les selectors 			///
+	//////////////////////////////
+	
+	//renvoie l'image source
+	public BufferedImage getSrc(){return this.source;}
+	//renvoie le sprite 
+	public BufferedImage getImg(){return this.img;}
+	//renvoie la coordonnée X
+	public int getX() { return dot.x;}
+	//renvoie la coordonnée Y 
+	public int getY(){ return dot.y;}
+
+
+	public static void main (String [] args) throws AWTException{
+		Game g = new Game ("http://www.jeux-flash-gratuits.biz/games/sushi-go-round.swf");
+		g.Start("/Users/sofiane/desktop/screenshot10.png");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		BufferedImage game = g.getScreen(10000);
+//		FindPicture test = new FindPicture(game,"/Users/sofiane/desktop/screenshot10.png");
+//		test.checkLine(test.getSrc(), test.getImg());
+//		test.checkColumn(test.getSrc(), test.getImg());
+//		if (test.getX()==-1 || test.getY()==-1)throw new RuntimeException("Sprite non detecté");
+//		else {
+//		System.out.println("is line working ? "+test.getY());
+//		System.out.println("is column working ? "+test.getX());
+//		me.delay(1000);
+//		me.mouseMove(0,0);
+//		me.mouseMove(test.getX(), test.getY());
+		//me.mousePress(InputEvent.BUTTON1_MASK);
+		//me.mouseRelease(InputEvent.BUTTON1_MASK);
+		//}	
+	
 	}
+
 
 }
--- Game.java
+++ Game.java
@@ -1,7 +1,11 @@
+package Suchi;
+import java.awt.AWTException;
 import java.awt.Desktop;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,6 +13,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.imageio.ImageIO;
+
+import Pictures.FindPicture;
 
 /////////////////////////////////////////////////////////////////////////////
 // Une classe Game qui demarre un jeu 									   //
@@ -41,18 +47,16 @@ public class Game {
 	/*methode qui prend un screen apres x temps
 	 * renvoit un screen deja cadré sur le jeu  
 	 */
-	public BufferedImage getScreen(int x)throws Exception {
-		BufferedImage img	 = null ;
-		Robot bot = new Robot();
-		bot.mouseMove(0,0);
+	public BufferedImage getScreen(int x){
+		BufferedImage img	 = null ; 
 
 		try{
 			Thread.sleep(x);	
 			img = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 		}catch (Exception e){} ;
 
-		if (img==null) throw new RuntimeException("Pas d'image");
-		return img.getSubimage(img.getWidth()/8,img.getHeight()/12,img.getWidth()-2*img.getWidth()/8,img.getHeight()-img.getHeight()/12);
+		if (img==null) throw new RuntimeException("pas d'image");
+		return img;//.getSubimage(img.getWidth()/8,img.getHeight()/12,img.getWidth()-2*img.getWidth()/8,img.getHeight()-img.getHeight()/12);
 		
 	}
 	
@@ -61,15 +65,37 @@ public class Game {
 	 */
 	public void createScreenImage (BufferedImage image){
 		try {
-			ImageIO.write(image, "png", new File("screenshot.png"));
+			ImageIO.write(image, "png", new File("/Users/sofiane/desktop/screenshot.png"));
 		} catch (IOException e) {
 		}
 	}
-
-	public static void main(String[] args)throws Exception {
-		Game g = new Game("http://www.jeux-flash-gratuits.biz/games/sushi-go-round.swf");
-		BufferedImage img = g.getScreen(12000);
-		g.createScreenImage(img);	
-	}
+	
+	public void Start (String srcPath) throws AWTException{
+		Robot me = new Robot();
+		BufferedImage game = this.getScreen(10000);
+		FindPicture test = new FindPicture(game,srcPath);
+		test.checkLine(test.getSrc(), test.getImg());
+		test.checkColumn(test.getSrc(), test.getImg());
+		if (test.getX()==-1 || test.getY()==-1)throw new RuntimeException("Sprite non detecté");
+		else {
+		//System.out.println("is line working ? "+test.getY());
+		//System.out.println("is column working ? "+test.getX());
+		me.delay(1000);
+		me.mouseMove(0,0);
+		me.mouseMove(test.getX(), test.getY());
+		me.mousePress(InputEvent.BUTTON1_MASK);
+		me.mouseRelease(InputEvent.BUTTON1_MASK);
+		}
+	     }
 	
 }
+
+
+
+
+/////////////////////////////////
+// pour tester cette classe    //////////////////////////////////////////////////////
+//Game g = new Game("http://www.jeux-flash-gratuits.biz/games/sushi-go-round.swf");//
+//BufferedImage img = g.getScreen(12000);////////////////////////////////////////////
+//g.createScreenImage(img);				 //
+///////////////////////////////////////////

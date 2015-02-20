01423b8200200ce70e891db7ab3794ea04693d10
Raphael Cohen <cohen917@gmail.com>
2015-02-20 22:55:02 +0100

    Extrait un screen de la fenetre du jeu.

---
+++ GameWindow.java
@@ -0,0 +1,120 @@
+import java.awt.Desktop;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+import javax.imageio.ImageIO;
+import javax.swing.event.MouseInputListener;
+
+import java.awt.event.InputEvent;
+import java.awt.event.MouseListener;
+
+
+public class GameWindow {
+	
+	public static void main(String [] args){
+		GameWindow window = new GameWindow();
+		window.returnCropped(window.getImg());
+		
+		//me.mouseMove(0,0);
+		}
+	
+	BufferedImage img;
+	
+	public GameWindow(){
+		
+		try{
+			Desktop d = Desktop.getDesktop();
+			d.browse(new URI("http://www.jeux-flash-gratuits.biz/games/sushi-go-round.swf"));		
+			Thread.sleep(10000);	
+			this.img = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+			ImageIO.write(img, "png", new File("Screen.png"));
+		}catch (Exception e){System.out.println("Exception: " + e);};
+		
+	}
+	
+	//on recupere le x du bord gauche de la fenêtre du jeu
+	
+	public int xGauche(BufferedImage img){
+		int x = 0, tmp = 0, compteur = 0;
+		int[] line = new int[img.getWidth()];
+		for(int i = 0; i< img.getWidth(); i++){
+			line[i] = img.getRGB(i, img.getHeight()/2);
+		}
+		for(int i = 0; i < line.length/2; i++){
+			if(line[i] == 0xFF000000){
+				tmp ++;
+				//System.out.println("noir" + i);
+			}
+			else if(tmp > compteur && line[i] != 0xFF000000){
+				compteur = tmp;
+				x = i;
+			}
+			else tmp = 0;
+			
+		}
+		x--; // le tableau commence à 0 mais pas les pixels de l'image
+		//System.out.println(x);
+		
+		
+		return x;
+	}
+	
+	public int xDroit(BufferedImage img){
+		
+		int x = 0, tmp = 0, compteur = 0;
+		int[] line = new int[img.getWidth()];
+		for(int i = 0; i< img.getWidth(); i++){
+			line[i] = img.getRGB(i, img.getHeight()/2);
+		}
+		
+		for(int i = img.getWidth()-1; i > line.length/2; i--){
+			
+			if(line[i] == 0xFF000000){
+				tmp ++;
+				//System.out.println("noir" + i);
+			}
+			else if(tmp > compteur && line[i] != 0xFF000000){
+				compteur = tmp;
+				x = i;
+			}
+			else tmp = 0;
+			
+		}
+		x++; // le tableau commence à 0 mais pas les pixels de l'image
+		//System.out.println(x);
+		
+		
+		return x;
+	}
+	
+	public int yHaut(BufferedImage img){
+		int x = this.xGauche(img);
+		//int y = 0;
+		int j = img.getHeight()/2;
+		
+		while(img.getRGB(x, j) == 0xFF000000 && j > 0)
+			j--;
+		
+		//System.out.println(j);
+		return j+1 ;
+	}
+	public BufferedImage returnCropped(BufferedImage img){
+		System.out.println(this.xGauche(img) + " " + this.xDroit(img)  + " " + this.yHaut(img) + " " + (this.xDroit(img)-this.xGauche(img)));
+		int hauteur = (3*(this.xDroit(img)-this.xGauche(img))/4);
+		BufferedImage gameWindow = this.img.getSubimage(this.xGauche(img), this.yHaut(img), this.xDroit(img)-this.xGauche(img), hauteur);
+		
+		try {
+			ImageIO.write(gameWindow, "png", new File("ScreenCrop.png"));
+		} catch (IOException e) {
+		}
+		return null;
+		
+	}
+	
+	public BufferedImage getImg(){return this.img;}
+}

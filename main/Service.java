996372df457c622356370f0c2f2eb24f6b310bdc
antwansZ <ant_78@live.fr>
2015-03-01 22:35:42 +0100

    J'ai update le code qui recherche la fenetre du jeu, normalement ca devrait marcher sur toutes les tailles et sur toutes les resolutions maintenant;

---
+++ main/Service.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.awt.AWTException;
+
+
+public class Service extends Thread{
+	
+	public Service(String nOClient) {
+		
+		super(nOClient);
+		
+	}
+	
+	public void run(){
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
